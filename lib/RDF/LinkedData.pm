8e1baba2fd64891500bd71f6ea14e707bdc71d01
Kjetil Kjernsmo <kjetil@startsiden.no>
2010-07-20 10:35:19 +0200

    Bump version and Trine 0.125

--- lib/RDF/LinkedData.pm
+++ lib/RDF/LinkedData.pm
@@ -12,11 +12,11 @@ RDF::LinkedData - Linked Data implementation default class
 
 =head1 VERSION
 
-Version 0.06
+Version 0.07
 
 =cut
 
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 
 
 =head1 SYNOPSIS
