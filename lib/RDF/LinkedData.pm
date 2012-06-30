22ae427ee38a2eb5da0a45baf07b75f3238af03b
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2012-06-30 02:14:50 +0200

    bump version

--- lib/RDF/LinkedData.pm
+++ lib/RDF/LinkedData.pm
@@ -36,11 +36,11 @@ RDF::LinkedData - A simple Linked Data implementation
 
 =head1 VERSION
 
-Version 0.53_1
+Version 0.54
 
 =cut
 
-our $VERSION = '0.53_1';
+our $VERSION = '0.54';
 
 
 =head1 SYNOPSIS
