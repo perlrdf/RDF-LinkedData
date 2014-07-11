d60ddd9d575a11a3857b4f826b0bda986813dc1d
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2014-07-11 23:22:59 +0200

    fix URI prefix bug

--- lib/RDF/LinkedData.pm
+++ lib/RDF/LinkedData.pm
@@ -322,9 +322,11 @@ sub response {
 			my $content = $self->_content($node, $type, $endpoint_path);
 			$response->headers->header('Vary' => join(", ", qw(Accept)));
 			if (defined($self->current_etag)) {
-				$self->current_etag =~ m|(^W/)|; # If the ETag is declared as weak, preserve that
-				my $weak = defined($1) ? $1 : '';
-				$response->headers->header('ETag' => '"' . $weak . md5_hex($self->current_etag . $content->{content_type}) . '"');
+			  my $weak = '';
+			  if ($self->current_etag =~ m|^W/|) { # If the ETag is declared as weak, preserve that
+				 $weak = 'W/';
+			  }
+			  $response->headers->header('ETag' => '"' . $weak . md5_hex($self->current_etag . $content->{content_type}) . '"');
 			}
 			$response->headers->content_type($content->{content_type});
 			$response->body(encode_utf8($content->{body}));
@@ -626,9 +628,11 @@ sub _void_content {
 		$etag = $self->_last_extvoid_mtime if ($self->void_config->{add_void});
 		$etag .= $self->last_etag if (defined($self->last_etag));
 		if ($etag) {
-			$etag =~ m|(^W/)|; # If the ETag is declared as weak, preserve that
-			my $weak = defined($1) ? $1 : '';
-			$response->headers->header('ETag' => '"' . $weak . md5_hex($etag . $ct) . '"');
+		  my $weak = '';
+		  if($etag =~ m|^W/|) { # If the ETag is declared as weak, preserve that
+			 $weak = 'W/';
+		  }
+		  $response->headers->header('ETag' => '"' . $weak . md5_hex($etag . $ct) . '"');
 		}
 		$response->headers->content_type($ct);
 		$response->body(encode_utf8($body));
