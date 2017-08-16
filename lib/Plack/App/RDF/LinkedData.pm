94eea2365bf59c8fc4847137d17a5ebbf2e47062
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2017-08-17 00:15:09 +0200

    Bump version

--- lib/Plack/App/RDF/LinkedData.pm
+++ lib/Plack/App/RDF/LinkedData.pm
@@ -12,11 +12,11 @@ Plack::App::RDF::LinkedData - A Plack application for running RDF::LinkedData
 
 =head1 VERSION
 
-Version 0.99_01
+Version 0.99_02
 
 =cut
 
- our $VERSION = '0.99_01';
+ our $VERSION = '0.99_02';
 
 
 =head1 SYNOPSIS
--- lib/RDF/LinkedData.pm
+++ lib/RDF/LinkedData.pm
@@ -32,11 +32,11 @@ RDF::LinkedData - A Linked Data server implementation
 
 =head1 VERSION
 
-Version 0.99_01
+Version 0.99_02
 
 =cut
 
- our $VERSION = '0.99_01';
+ our $VERSION = '0.99_02';
 
 
 =head1 SYNOPSIS
